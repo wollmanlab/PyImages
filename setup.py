d211b096a3fed697264a4a49ab745c2f8b7cbd4a
rfor10 <rforeman@ucsd.edu>
2018-02-27 18:52:15 -0800

    Create setup.py
    setup file to install package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='pyimages',
+    version='0.1',
+    description='Code for live-cell image analysis in python', 
+    url='https://github.com/rfor10/pyimages',
+    author='Robert Foreman',
+    author_email='rfor10@gmail.com',
+    license='MIT',
+    zip_safe=False)
