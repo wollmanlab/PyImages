73db7c65bc84678fd656ed418710c8814f7faf5f
jahdoos <rfor10@gmail.com>
2018-10-03 01:08:06 -0700

    tracking in now LAPJV and registration designed

--- cell_label.py
+++ cell_label.py
@@ -1,4 +1,5 @@
 # Segment imports
+import warnings
 import numpy
 from scipy.ndimage import binary_fill_holes, binary_erosion, distance_transform_edt, binary_closing, binary_dilation
 from scipy.ndimage import binary_dilation
@@ -6,13 +7,15 @@ from skimage.morphology import remove_small_objects, disk, erosion, watershed
 from skimage import filters, img_as_uint
 from skimage.feature import peak_local_max
 from skimage.measure import label, regionprops
-from skfmm import distance
+# from skfmm import distance
+# Tracking imports
+import lap
 
 # Cell Label imports
 import numpy
 import numpy as np
 from scipy.sparse import bsr_matrix
-from scipy.spatial import KDTree
+from scipy.spatial import KDTree, distance_matrix, distance
 from collections import Counter, defaultdict
 
 def parseVarargin(varargin, arg):
@@ -37,97 +40,148 @@ class CellLabel(object):
         self.useCC = False
         self.cell_ids = []
         
-        self.Labels = {}
+        self.Labels = defaultdict(dict)
     
-    def getXY(self, T, varargin={}):
-        if T not in self.Labels:
-            if len(self.Labels)>0:
-                print('Timestamp not found in labels. Returning nearest.')
-        print('Not Implemented')
+    def getXY(self, T, labeltype='nuc', return_as='dict', varargin={}):
+        if T not in self.Labels[labeltype]:
+            pass
+        lbl = self.Labels[labeltype][T]
+        regprops = regionprops(lbl)
+        if return_as=='dict':
+            return {p['label']: p['centroid'] for p in regprops}
 
+    def relabel(self, tracks, labeltype='nuc'):
+        lbls = self.Labels[labeltype]
+        for tp_i, l in lbls.items():
+            newlbl = np.zeros_like(l)
+            props = regionprops(l)
+            lbl_coords = {p.centroid: p.coords for p in props}
+            for tp_centroid, newlbl_id in tracks.items():
+                for tp_j, centroid in tp_centroid:
+                    if tp_i == tp_j:
+                        for coord in lbl_coords[centroid]:
+                            newlbl[coord[0], coord[1]] = newlbl_id
+                    else:
+                        continue
+            lbls[tp_i] = newlbl
+
+    
+    def trackLabels(self, labeltype='nuc', max_displacement=50,
+                    max_discontinuity=3):
+        lbls = {tp: list(self.getXY(tp).values())
+                for tp in self.Labels[labeltype].keys()}
+        
+        if len(lbls)==0:
+            raise ValueError("No labels to track.")
+        elif len(lbls)==1:
+            warnings.warn("There is only one label so nothing was tracked.")
+            return
+        timepoints = sorted(list(lbls.keys()))
+        consecutive_tp_pairs = [(timepoints[i], timepoints[i+1])
+                        for i in range(len(timepoints)-1)]
+        tp2idx = {tp:i for i, tp in enumerate(timepoints)}
+        trackdict = {}
+        # Loop over all consecutive timepoints and use LAP to track
+        # Cost is simply the distance of nuclei centroids
+        for ti, tj in consecutive_tp_pairs:
+            lbl_i, lbl_j = lbls[ti], lbls[tj] # Assuming these are centroids
+            cost_matrix = distance_matrix(lbl_i, lbl_j)
+            total_cost, column2row, row2column = lap.lapjv(cost_matrix,
+                                                           cost_limit=max_displacement,
+                                                           extend_cost=True)
+            # Parse LAP output to build dictionary of paired items
+            for col, row in enumerate(column2row):
+                if row == -1:
+                    trackdict[(ti, tuple(lbl_i[col]))] = -1
+                else:
+                    trackdict[(ti, tuple(lbl_i[col]))] = (tj, tuple(lbl_j[row]))
+        # Merge all pairs that link together into merged track
+        # Pairs are linked if they connected through a common element.
+        merged_tracks = []
+        for k, v in list(trackdict.items()):
+            if v in trackdict: # Condition means v links to another pair from different timepoint
+                merged_track = [k] # Start building the merged track of linked pairs
+                while v in trackdict: # Keep building until no more
+                    merged_track += [v]
+                    trackdict.pop(k)
+                    k = v
+                    v = trackdict[v]
+                trackdict.pop(k)
+                if not v == -1: # If last element is unpaired add just the key
+                    merged_track += [v] # else add the last linked item
+                merged_tracks.append(merged_track)
+        # Remove complete tracks from further linking (i.e. all timepoints are connected)
+        incomplete_tracks = [t for t in merged_tracks if len(t)<len(lbls)]
+        tracks = incomplete_tracks
+        # Build data structures for gap closing cost matrix creation
+        track_starts = np.array([i[0][0] for i in tracks])
+        track_ends = np.array([i[-1][0] for i in tracks])
+
+        track_xy_start = np.array([i[0][1] for i in tracks])
+        track_xy_end = np.array([i[-1][1] for i in tracks])
+        
+        # Try to link more tracks by allowing gaps within track
+        gap_cost_mat = np.ones((n, n))*(max_displacement+1) # this should be bigger than max_displacement
+        for idx, trck in enumerate(tracks):
+            tstart = trck[0][0]
+            tend = trck[-1][0]
+            xy_start = trck[0][1]
+            xy_end = trck[-1][1]
+            tracks_starting_after_this_ends = np.where((track_starts>tend) & (track_starts<tend+max_discontinuity))[0]
+            if len(tracks_starting_after_this_ends)>0:
+                start_tree = KDTree(track_xy_start[tracks_starting_after_this_ends])
+                possmerge_start = start_tree.query(xy_end, k=50, distance_upper_bound=max_displacement)
+                for d, tidx in zip(possmerge_start[0], possmerge_start[1]):
+        #             if idx == tidx:
+        #                 continue
+                    if d>max_displacement:
+                        break
+                    else:
+                        gap_cost_mat[idx, tracks_starting_after_this_ends[tidx]] = d
+            tracks_ending_before_this_track = np.where((track_ends<tstart) & (track_ends>tstart-max_discontinuity))[0]
+            if len(tracks_ending_before_this_track)>0:
+                end_tree = KDTree(track_xy_end[tracks_ending_before_this_track])
+                possmerge_end = end_tree.query(xy_start, k=50, distance_upper_bound=max_displacement)
+                for d, tidx in zip(possmerge_end[0], possmerge_end[1]):
+        #             if idx == tidx:
+        #                 continue
+                    if d>max_displacement:
+                        break
+                    else:
+                        cc.append(d)
+                        ii.append(idx)
+                        kk.append(tidx)
+                        gap_cost_mat[idx, tracks_ending_before_this_track[tidx]] = d
+        a,b,c = lap.lapjv(gap_cost_mat, cost_limit=max_displacement,
+                          extend_cost=True)
+        leftover_merges = []
+        for idx, i in enumerate(c):
+            if i == -1:
+                continue
+            track_i = tracks[idx]
+            track_j = tracks[i]
+            leftover_merges.append(track_i+track_j)
+        # Build final output and ensure there are no duplicate tracks
+        final_tracks = {}
+        track_id = 1
+        for track in merged_tracks+leftover_merges:
+            track = tuple(sorted(track, key=lambda x: x[0]))
+            if track not in final_tracks:
+                final_tracks[track] = track_id
+                track_id += 1
+        self.tracks = final_tracks
+        return final_tracks
+    #print(merged_xys)
+    
     def addLabel(self, newlabel, labeltype, T, varargin={}):
         arg = dict()
         arg['posname'] = 'None'
         arg['maxcelldistance'] = 25
-        arg['relabel'] = 'nearest'
+        arg['relabel'] = None
         arg = parseVarargin(varargin, arg)
         
         newlabel = newlabel.astype(numpy.uint16)
-        
-        if arg['relabel'] == 'nearest':
-            nlabels = len(self.Labels)
-            if nlabels==0:
-                self.Labels[T] = bsr_matrix(newlabel)
-                return
-            elif nlabels==1:
-                if self.Labels.keys()[0] == T:
-                    self.Labels[T] = bsr_matrix(newlabel)
-                    print('Warning - added a newlabel with same timestamp as preexisting label. Preexisting label was overwritten.')
-                else:
-                    existing_t = self.Labels.keys()
-                    dist2existing = numpy.abs(numpy.subtract(existing_t, T))
-                    nearest_t = existing_t[numpy.argmin(dist2existing)]
-                    self.Labels[T] = bsr_matrix(self._relabel_nearest(newlabel, nearest_t))
-            elif nlabels>1:
-                if T in self.Labels.keys():
-                    print('Warning - added a newlabel with same timestamp as preexisting label. Preexisting label was overwritten.')
-                    existing_t = self.Labels.keys()
-                    dist2existing = numpy.abs(numpy.subtract(existing_t, T))
-                    secondNearest_t = existing_t[numpy.argsort(dist2existing)[1]]
-                    self.Labels[T] = bsr_matrix(self._relabel_nearest(newlabel, secondNearest_t))
-                else:
-                    existing_t = self.Labels.keys()
-                    dist2existing = numpy.abs(numpy.subtract(existing_t, T))
-                    nearest_t = existing_t[numpy.argmin(dist2existing)]
-                    self.Labels[T] = bsr_matrix(self._relabel_nearest(newlabel, nearest_t))
-            self.tracked = True
-        cell_ids = set(range(100000))
-        for t, label in self.Labels.items():
-            label = label.toarray()
-            ids = set(label.flatten())
-            cell_ids = cell_ids.intersection(set(ids))
-        cell_ids.difference_update(set([0]))
-        self.cell_ids = list(cell_ids)
-            
-    def _relabel_nearest(self, newlabel, nearest_t, varargin={}):
-        arg = dict()
-        arg['maxcelldistance'] = 25
-        nearest_label = self.Labels[nearest_t]
-        if isinstance(nearest_label, bsr_matrix):
-            nearest_label = nearest_label.toarray()
-        nearest = [(p.label, p.centroid, p.coords) for p in regionprops(nearest_label)]
-        nearest_label, nearest_xy, coords = zip(*nearest)
-        new = [(p.label, p.centroid, p.coords) for p in regionprops(newlabel)]
-        new_label, new_xy, _ = zip(*new)
-        labels_coords = {k:v for k, _, v in new}
-        knntree = KDTree(nearest_xy)
-        dists, idx = knntree.query(new_xy, k=2, eps=arg['maxcelldistance'])
-        qdata = zip(new_label, dists, idx)
-        label_map = {}
-        for newlbl_idx, d, j in qdata:
-            if d[0]>arg['maxcelldistance']:
-                label_map[newlbl_idx] = 0
-            else:
-                label_map[newlbl_idx] = nearest_label[j[0]]
-        counts = Counter(label_map.values())
-        for l, c in counts.items():
-            if c>1:
-                label_map[l] = 0
-        if 0 in counts:
-            del counts[0]
-        if 0 in label_map:
-            del label_map[0]
-        if counts.most_common(1)>1:
-            print('Warning two cells were assigned the same label.', counts.most_common(4))
-        for k, v in label_map.items():
-            try:
-                coords = labels_coords[k]
-            except:
-                print(k)
-            for x, y in coords:
-                newlabel[x, y] = v
-        
-        return bsr_matrix(newlabel)
+        self.Labels[labeltype][T] = newlabel
     
     def applyFuncPerLabel(self, stk, T, func=numpy.mean, varargin={},
                           outtype='matrix'):
@@ -242,3 +296,33 @@ def segmentNucleiOnly(nuc, varargin={}):
 #         lbls = binary_opening(lbls, disk(4))
         NucLabels[:,:,i] = lbls
     return NucLabels
+
+class Registration(object):
+    def __init__(self, tforms=dict()):
+        self.tforms=tforms
+    def find_translation(self, src, dest, timepoint):
+        tvec = register_translation(dest, src)
+        tform = AffineTransform(translation=(tvec[0][1], tvec[0][0]))
+        self.tforms[timepoint] = tform
+        return tform
+    def apply_tform(self, dest, dest_timepoint=None, order=1):
+        dest = dest.copy()
+        if isinstance(dest_timepoint, skimage.transform._geometric.AffineTransform):
+            pass
+        else:
+            dest_timepoint = self.tforms[dest_timepoint]
+        dest = warp(dest, dest_timepoint, preserve_range=True, order=order)
+        return dest
+    def map_and_apply_tforms(self, dest_stack, dest_timepoints, grouping='nearest', order=1):
+        dest_stack = dest_stack.copy()
+        source_timepoints = list(self.tforms.keys())
+        if grouping == 'nearest':
+            grouping = []
+            for dest_tp in dest_timepoints:
+                tdist = np.abs(np.subtract(source_timepoints, dest_tp))
+                closest_source_tp = np.argmin(tdist)
+                grouping.append(source_timepoints[closest_source_tp])
+        dest_stack = np.stack([self.apply_tform(dest_stack[:,:,i], self.tforms[grouping[i]], order=order) 
+                              for i in range(dest_stack.shape[2])])
+        return dest_stack
+    
