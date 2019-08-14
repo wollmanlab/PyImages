51e35418892d65e56aff7a262796bb23543a9cd3
alonyan <alonyan1@gmail.com>
2019-08-14 09:07:07 -0700

    add QT based stackshow to metadata.py

--- metadata.py
+++ metadata.py
@@ -181,10 +181,10 @@ class Metadata(object):
             hybes = [i.split('_')[0] for i in acqs]
             keepers = []
             for i in hybes:
-            	if i in kwargs['hybe']:
-            		keepers.append(True)
-            	else:
-            		keepers.append(False)
+                if i in kwargs['hybe']:
+                    keepers.append(True)
+                else:
+                    keepers.append(False)
             image_subset_table = image_subset_table[keepers]
         # Group images and sort them then extract filenames of sorted images
         image_subset_table.sort_values(sortby, inplace=True)
@@ -282,3 +282,93 @@ class Metadata(object):
         np.place(img, img<0, 0)
         np.place(img, img>bitdepth, bitdepth)
         return img
+
+from numba import jit
+@jit(nopython = True)
+def DownScale(imgin): #use 2x downscaling for scrol speed   
+        #imgout = trans.downscale_local_mean(imgin,(Sc, Sc))
+    imgout = (imgin[0::2,0::2]+imgin[1::2,0::2]+imgin[0::2,1::2]+imgin[1::2,1::2])/4
+    return imgout
+
+def stkshow(data):
+    from pyqtgraph.Qt import QtCore, QtGui
+    import pyqtgraph as pg
+    import sys
+    import skimage.transform as trans
+
+    
+    # determine if you need to start a Qt app. 
+    # If running from Spyder, the answer is a no.
+    # From cmd, yes. From nb, not sure actually.
+    if not QtGui.QApplication.instance():
+        app = QtGui.QApplication([])
+    else:
+        app = QtGui.QApplication.instance()
+        
+    ## Create window with ImageView widget
+    win = QtGui.QMainWindow()
+    win.resize(800,800)
+    imv = pg.ImageView()
+    win.setCentralWidget(imv)
+    win.show()
+    win.setWindowTitle('Fetching image stack...')
+    
+    
+    
+    
+    resizeflg = 0;
+    maxxysize = 800;
+    maxdataxySc = np.floor(max(data.shape[0],data.shape[1])/maxxysize).astype('int')
+    if maxdataxySc>1:
+        resizeflg = 1;
+
+    if len(data.shape)==4:#RGB assume xytc
+        if data.shape[3]==3 or data.shape[3]==4:
+            if resizeflg:
+                dataRs = np.zeros((np.ceil(data.shape/np.array([maxdataxySc,maxdataxySc,1,1]))).astype('int'),dtype = 'uint16')    
+                for i in range(0,data.shape[2]):
+                    for j in range(0,data.shape[3]):
+                        dataRs[:,:,i,j] = DownScale(data[:,:,i,j])
+                dataRs = dataRs.transpose((2,0,1,3))
+            else:
+                dataRs = data;
+                dataRs = dataRs.transpose((2,0,1,3))
+        else:
+            sys.exit('color channel needs to be RGB or RGBA')
+    elif len(data.shape)==3:
+        if resizeflg:
+            dataRs = np.zeros((np.ceil(data.shape/np.array([maxdataxySc,maxdataxySc,1]))).astype('int'),dtype = 'uint16')    
+            for i in range(0,data.shape[2]):
+                dataRs[:,:,i] = DownScale(data[:,:,i])
+            dataRs = dataRs.transpose([2,0,1])
+        else:
+            dataRs = data;
+            dataRs = dataRs.transpose([2,0,1])
+                
+    elif len(data.shape)==2:
+        if resizeflg:
+            dataRs = np.zeros((np.ceil(data.shape/np.array([maxdataxySc,maxdataxySc]))).astype('int'),dtype = 'uint16')
+            dataRs = DownScale(data)
+        else:
+            dataRs = data;
+    else:
+        print('Data must be 2D image or 3D image stack')
+    
+
+    
+    # Interpret image data as row-major instead of col-major
+    pg.setConfigOptions(imageAxisOrder='row-major')
+    
+
+    win.setWindowTitle('Stack')
+    
+    ## Display the data and assign each frame a 
+    imv.setImage(dataRs)#, xvals=np.linspace(1., dataRs.shape[0], dataRs.shape[0]))
+
+    ##must return the window to keep it open
+    return win
+    ## Start Qt event loop unless running in interactive mode.
+    if __name__ == '__main__':
+        import sys
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
